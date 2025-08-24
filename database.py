347bd1ee868cc2a053cae582b905e708f85e6c06
alexisportillo-admin <alexisportillo95@outlook.com>
2025-08-24 08:48:04 -0500

    signa: se prepara archivos para desplegue en render

--- database.py
+++ database.py
@@ -11,7 +11,11 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 # Crear el engine
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    # Para PostgreSQL en producción
+    engine = create_engine(DATABASE_URL)
 
 # Crear la sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
